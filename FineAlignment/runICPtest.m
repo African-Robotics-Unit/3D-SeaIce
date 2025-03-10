c3b08f90c67c5a2a98bb182258306874468c8afe
Agi23 <agoritsa.spirakis@gmail.com>
2025-03-10 16:27:20 +0200

    ICP transform outputs

--- FineAlignment/runICPtest.m
+++ FineAlignment/runICPtest.m
@@ -1,10 +1,11 @@
 addpath(genpath("C:\Users\agori\Documents\MATLAB\3D-Sea-Ice\FineAlignment\GliraICP"));
 %addpath(genpath('C:\Users\agori\Documents\MATLAB\MSc\CRUISE-DATA\LiDAR Extraction'));
 %icptest=runGliraICP('C:\Users\agori\Documents\MATLAB\MSc\appDesign\ICPtestA\P3clean\','C:\Users\agori\Documents\MATLAB\MSc\appDesign\ICPtestA\P3ICP\');
-icptest=runGliraICP('C:\Users\agori\Documents\MATLAB\MSc\Validation\2blocksROS2\BeforeAlignment\','C:\Users\agori\Documents\MATLAB\MSc\Validation\2blocksROS2\AfterAlignment\');
+icptest=runGliraICP('C:\Users\agori\Documents\MATLAB\MSc\Validation\2blocksROS2\BeforeAlignment\','C:\Users\agori\Documents\MATLAB\MSc\Validation\2blocksROS2\AfterTrans\');
 figure; icptest.plot('Color', 'by PC');
 
-ICPOptions.NoOfTransfParam          = 6; %Rigid body transform
+%ICPOptions.NoOfTransfParam          = 6; %Rigid body transform
+ICPOptions.NoOfTransfParam          = 3; %Rigid body transform
 ICPOptions.UniformSamplingDistance  = 0.2;
 ICPOptions.PlaneSearchRadius        = 0.1;
 ICPOptions.MaxRoughness             = 0.02;
